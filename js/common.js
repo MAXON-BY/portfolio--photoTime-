9dafd58cc472c1c79fd3c745c83ab99f61994e43
MAXON-BY <maxon.pro2015@gmail.com>
2015-05-09 17:45:49 +0300

    1
    1

---
+++ js/common.js
@@ -0,0 +1,21 @@
+    $("#owl-demo").owlCarousel({
+
+        autoPlay: 3000, //Set AutoPlay to 3 seconds
+
+        navigation : true, // Show next and prev buttons
+        navigationText : [" "," "],
+        items : 2,
+        itemsDesktop: false
+
+    });
+
+    $("#owl-demo1").owlCarousel({
+
+        autoPlay: 5000, //Set AutoPlay to 5 seconds
+
+        navigation : true, // Show next and prev buttons
+        navigationText : [" "," "],
+        items : 1,
+        itemsDesktop: false
+
+    });
